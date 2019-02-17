998a9f87a16e7261a08c91bcd1366099ad0cadab
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-17 21:01:02 +0530

    Update set1-4.c

--- set1-4.c
+++ set1-4.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include<string.h>
 int main(void) {
-	char s[50];
-	scanf("%s",s);
-	strcat(s,".");
-	printf("%s",s);
+	char a[50];
+	scanf("%s",a);
+	strcat(a,".");
+	printf("%s",a);
 	return 0;
 }
