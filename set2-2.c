2c92b276140e7d8c77a4937564f110a52eae1e0a
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-18 21:39:35 +0530

    Create set2-2.c

---
+++ set2-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+	int n,s[10],k,temp=0,i,j;
+	scanf("%d %d\n",&n,&k);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&s[i]);
+	}
+	for(i=0;i<k;i++)
+	{
+	    temp=a[n-1];
+	    for(j=n-1;j>=0;j--)
+	    {
+	       s[j]=s[j-1];
+	    }
+	    s[0]=temp;
+	}
+	for(i=0;i<n;i++)
+	{
+	    printf("%d ",s[i]);
+	}
+	
+	return 0;
+}
