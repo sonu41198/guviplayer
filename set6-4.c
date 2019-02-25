92dae3663fe77ef030c7a88e76be09d05d10d208
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-02-25 11:42:40 +0530

    Create set6-4.c

---
+++ set6-4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char str1[20],str2[20];
+	int i,f;
+	scanf("%s %s",str1,str2);
+	int l1,l2;
+	l1=strlen(str1);
+	l2=strlen(str2);
+	if(l1==l2)
+	{
+		for(i=0;i<l1;i++)
+		{
+			if(str1[i]=str2[i])
+			{
+				f=0;
+			}
+			else
+			{
+				f=1;
+				break;
+			}
+		}
+		if(f==1)
+		{
+			printf("no");
+		}
+		else
+		{
+			printf("yes");
+		}
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
