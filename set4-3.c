a1b365b9d3d2d0ad988b5be9f2b8d6908d690bd4
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-03-10 20:24:14 +0530

    Create set4-3.c

---
+++ set4-3.c
@@ -0,0 +1,27 @@
+{
+    int a[100][100],i,j,n,count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    for(j=0;j<n;j++)
+    {
+    scanf("%d",&a[i][j]);
+    }
+    }
+    for(i=0;i<n;i++)
+    {
+    for(j=0;j<n;j++)
+    
+    {
+    if(a[i][j]==1)
+    {
+    if(a[i-1][j]==0&&a[i][j-1]==0&&a[i][j+1]==0&&a[i+1][j]==0)
+    {
+     count++;
+    }
+    }
+    }
+    }
+    printf("%d",count);
+    return 0;
+    }
