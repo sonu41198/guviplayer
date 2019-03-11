5ef2ee9ff9c1fe1289673188b968c98df395963e
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-03-11 06:39:17 +0530

    Create set6-8.c

---
+++ set6-8.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+char s[50],a[10];
+int i,j;
+for(i=0;i!='\0';i++)
+{
+scanf("%c",&a[i])
+}
+for(j=0;j!='\0';j++)
+{
+scanf("%[^\n]",&s[j]);
+}
+if(s[j]==a[i])
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
