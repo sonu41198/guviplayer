97121ab8c4b13647a070318c9d2309cfe0579bf0
sonu41198 <46063063+sonu41198@users.noreply.github.com>
2019-03-14 22:10:28 +0530

    Create set10-8.c

---
+++ set10-8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char a[100000];
+    int i,l,c=0,k;
+    gets(a);
+    scanf("%d",&k);
+    l=strlen(a);
+    for(i=0;i<l;i++) 
+     if(a[i]>=0 && a[i]<=k)
+             c++;
+     if(l==c)
+        printf("yes");
+    else
+         printf("no");
+    
+}
